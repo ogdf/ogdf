b31402c6675def718fbe18084e187cea9a83b5df
Christoph Schulz <schulzcbs@gmail.com>
2013-05-17 14:26:45 +0200

    Run some graph generators.

--- test/main.cpp
+++ test/main.cpp
@@ -42,10 +42,72 @@
  ***************************************************************/
 
 
+#include "ogdf/basic/Graph.h"
+#include "ogdf/basic/graph_generators.h"
 #include <iostream>
+#include <cstdlib>
+
+double random()
+{
+  return static_cast<double>(rand()) / RAND_MAX;
+}
+
+void randomGraph(ogdf::Graph &G, size_t index)
+{
+  int n = static_cast<int>(random() * 10 + 5);
+  int m = std::max(n + 1, static_cast<int>((random() * n * (n - 1) / 2)));
+  double p = 0.618;
+  switch (index) {
+  case 0:
+      G.clear();
+      ogdf::randomGraph(G, n, m);
+      break;
+  case 1:
+      G.clear();
+      ogdf::randomSimpleGraph(G, n, m);
+      break;
+  case 2:
+      G.clear();
+      ogdf::randomBiconnectedGraph(G, n, m);
+      break;
+  case 3:
+      G.clear();
+      ogdf::randomTriconnectedGraph(G, n, p, 1.0 - p);
+      break;
+  case 4:
+      G.clear();
+      ogdf::randomTree(G, n);
+      break;
+  case 5:
+      G.clear();
+      ogdf::randomTree(G, n, n / 2, 1024);
+      break;
+  case 6:
+      G.clear();
+      ogdf::randomHierarchy(G, n, m, true, false, true);
+      break;
+  case 7:
+      G.clear();
+      ogdf::randomDiGraph(G, n, p);
+      break;
+  default:
+    return;
+  }
+}
 
 int main(int argc, char **argv)
 {
-  std::cout << "TODO: NYI" << std::endl;
+  srand(8609);
+  ogdf::Graph G;
+
+  std::cout << "Testing graph generators" << std::endl;
+  for (size_t i = 0; i < 8; i++) {
+    randomGraph(G, i);
+  }
+
+  // TODO: write graph file (for crash debugging).
+
+  // TODO: test layouts (requires factory mechanism).
+
   return 0;
 }
