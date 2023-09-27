49491619f6844da3b7a9ad21d3db5e58afcd579e
Simon Dominik Niko Fink <simon.fink@uni-passau.de>
2023-09-27 22:42:43 +0200

    allow setting the value of the `-march` compiler flag

--- cmake/compiler-specifics.cmake
+++ cmake/compiler-specifics.cmake
@@ -15,7 +15,7 @@ endif()
 # use native arch (ie, activate things like SSE)
 if(CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang" AND NOT ${CMAKE_SYSTEM_PROCESSOR} MATCHES "^arm")
   # cannot use add_definitions() here because it does not work with check-sse3.cmake
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -march=native")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -march=${OGDF_ARCH}")
 endif()
 
 # set default warning flags for OGDF and tests
