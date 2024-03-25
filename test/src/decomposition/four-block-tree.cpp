66fd6cc2dad016577df6228d0474847606c1d5e2
Parranoh <minerva.schlau@hotmail.com>
2024-03-25 17:42:08 +0100

    add license to test

--- test/src/decomposition/four-block-tree.cpp
+++ test/src/decomposition/four-block-tree.cpp
@@ -1,3 +1,34 @@
+/** \file
+ * \brief Tests for functionality from ogdf/decomposition/FourBlockTree.h
+ *
+ * \author Gregor Diatzko
+ *
+ * \par License:
+ * This file is part of the Open Graph Drawing Framework (OGDF).
+ *
+ * \par
+ * Copyright (C)<br>
+ * See README.md in the OGDF root directory for details.
+ *
+ * \par
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * Version 2 or 3 as published by the Free Software Foundation;
+ * see the file LICENSE.txt included in the packaging of this file
+ * for details.
+ *
+ * \par
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * \par
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, see
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+
 #include <ogdf/basic/AdjEntryArray.h>
 #include <ogdf/basic/EdgeArray.h>
 #include <ogdf/basic/Graph_d.h>
