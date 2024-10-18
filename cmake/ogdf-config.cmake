914c5716a5afda3f036adfe3705987590f96fafd
Simon D Fink <finksim@fim.uni-passau.de>
2024-10-18 13:30:16 +0200

    try to fix backtrace library dependency

--- cmake/ogdf-config.cmake
+++ cmake/ogdf-config.cmake
@@ -1,5 +1,13 @@
 # This file is to be included by user code using find_package()
 
+if("@OGDF_USE_ASSERT_EXCEPTIONS_WITH_STACK_TRACE@" STREQUAL "ON_LIBDW")
+    find_package(Libdw)
+elseif("@OGDF_USE_ASSERT_EXCEPTIONS_WITH_STACK_TRACE@" STREQUAL "ON_LIBBFD")
+    find_package(Libbfd)
+elseif("@OGDF_USE_ASSERT_EXCEPTIONS_WITH_STACK_TRACE@" STREQUAL "ON_LIBUNWIND")
+    find_package(Libunwind)
+endif()
+
 include("${CMAKE_CURRENT_LIST_DIR}/CoinTargets.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/OgdfTargets.cmake")
 
