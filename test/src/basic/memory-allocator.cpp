993b05fdcbaecd3e6fb1b130c775687284b2fea8
Simon D Fink <finksim@fim.uni-passau.de>
2024-12-19 11:43:36 +0100

    add more tests for PoolMemoryAllocator

--- test/src/basic/memory-allocator.cpp
+++ test/src/basic/memory-allocator.cpp
@@ -30,11 +30,17 @@
  */
 
 #include <ogdf/basic/System.h>
+#include <ogdf/basic/Thread.h>
+#include <ogdf/basic/basic.h>
 #include <ogdf/basic/memory.h>
+#include <ogdf/basic/memory/PoolMemoryAllocator.h>
 
+#include <algorithm>
 #include <cstddef>
 #include <functional>
+#include <random>
 #include <string>
+#include <vector>
 
 #include <testing.h>
 
@@ -73,4 +79,165 @@ void describeMemoryManager(const std::string& name) {
 go_bandit([] {
 	describeMemoryManager<OGDFObject>("OGDF");
 	describeMemoryManager<MallocObject>("Malloc");
+
+#define OBJ_SIZE 128
+
+	describe(
+			"PoolMemoryManager",
+			[] {
+				std::vector<OGDFObject<OBJ_SIZE>*> objects;
+				const size_t MAX = 1024;
+				const size_t ALL_OBJS_SIZE = MAX * sizeof(OGDFObject<OBJ_SIZE>);
+				objects.reserve(MAX);
+
+				it("allocates memory", [&] {
+					auto get_used = [] {
+						return OGDF_ALLOCATOR::memoryAllocatedInBlocks()
+								- OGDF_ALLOCATOR::memoryInGlobalFreeList()
+								- OGDF_ALLOCATOR::memoryInThreadFreeList();
+					};
+					size_t used_before = get_used();
+					for (int i = 0; i < MAX; ++i) {
+						objects.push_back(new OGDFObject<OBJ_SIZE>());
+					}
+					AssertThat(get_used() - used_before, IsGreaterThanOrEqualTo(ALL_OBJS_SIZE));
+				});
+
+				it("grows its free list when objects are deleted", [&] {
+					std::vector<size_t> sizes_old;
+					PoolMemoryAllocator::getThreadFreeListSizes(sizes_old);
+
+					size_t mAllocatedInBlocks = PoolMemoryAllocator::memoryAllocatedInBlocks();
+					size_t mInGlobalFreeList = PoolMemoryAllocator::memoryInGlobalFreeList();
+					size_t mInThreadFreeList = PoolMemoryAllocator::memoryInThreadFreeList();
+					std::shuffle(objects.begin(), objects.end(), std::mt19937(randomSeed()));
+					for (auto object : objects) {
+						delete object;
+					}
+					AssertThat(PoolMemoryAllocator::memoryAllocatedInBlocks(),
+							Equals(mAllocatedInBlocks));
+					AssertThat(PoolMemoryAllocator::memoryInGlobalFreeList(),
+							Equals(mInGlobalFreeList));
+					AssertThat(PoolMemoryAllocator::memoryInThreadFreeList(),
+							IsGreaterThan(mInThreadFreeList));
+					AssertThat(PoolMemoryAllocator::memoryInThreadFreeList() - mInThreadFreeList,
+							Equals(ALL_OBJS_SIZE));
+
+					std::vector<size_t> sizes_new;
+					PoolMemoryAllocator::getThreadFreeListSizes(sizes_new);
+					AssertThat(sizes_new.at(OBJ_SIZE) - sizes_old.at(OBJ_SIZE), Equals(MAX));
+					sizes_new[OBJ_SIZE] = sizes_old[OBJ_SIZE];
+					AssertThat(sizes_new, Equals(sizes_old));
+
+					objects.clear();
+				});
+
+				it("correctly defragments its thread free list", [&] {
+					PoolMemoryAllocator::defragThread();
+					for (int i = 0; i < MAX; ++i) { // first set of objects from main thread for later
+						objects.push_back(new OGDFObject<OBJ_SIZE>());
+					}
+					AssertThat(std::is_sorted(objects.begin(), objects.end()), IsTrue());
+				});
+
+				std::vector<size_t> thread_sizes_old;
+				PoolMemoryAllocator::getThreadFreeListSizes(thread_sizes_old);
+				std::vector<size_t> global_sizes_old;
+				PoolMemoryAllocator::getGlobalFreeListSizes(global_sizes_old);
+				std::vector<size_t> side_thread_sizes_new;
+
+				ogdf::Thread([&] {
+					it("handles allocations and deletions mixed between threads", [&] {
+						for (int i = 0; i < MAX; ++i) { // second set of objects from side thread
+							objects.push_back(new OGDFObject<OBJ_SIZE>());
+						}
+						std::shuffle(objects.begin(), objects.end(), std::mt19937(randomSeed()));
+						AssertThat(objects.size(), Equals(MAX * 2));
+						for (int i = 0; i < MAX; ++i) { // half of objects deleted in side thread
+							delete objects.back();
+							objects.pop_back();
+						}
+						AssertThat(objects.size(), Equals(MAX));
+						PoolMemoryAllocator::getThreadFreeListSizes(side_thread_sizes_new);
+					});
+				}).join();
+				it("handles deletions of objects from other threads", [&] {
+					for (auto object : objects) { // last half of objects deleted in main thread
+						delete object;
+					}
+					objects.clear();
+				});
+
+				it("hands thread memory over to global pool on thread termination", [&] {
+					std::vector<size_t> thread_sizes_new;
+					PoolMemoryAllocator::getThreadFreeListSizes(thread_sizes_new);
+					std::vector<size_t> global_sizes_new;
+					PoolMemoryAllocator::getGlobalFreeListSizes(global_sizes_new);
+
+#ifdef OGDF_MEMORY_POOL_NTS
+					AssertThat(thread_sizes_new.at(OBJ_SIZE), IsGreaterThanOrEqualTo(MAX * 2));
+					AssertThat(global_sizes_new.empty(), IsTrue());
+#else
+			AssertThat(thread_sizes_new.at(OBJ_SIZE),
+					Equals(thread_sizes_old.at(OBJ_SIZE) + MAX));
+			AssertThat(global_sizes_new.at(OBJ_SIZE),
+					Equals(global_sizes_old.at(OBJ_SIZE) + side_thread_sizes_new.at(OBJ_SIZE)));
+#endif
+				});
+
+
+				it(
+						"correctly defragments its global free list",
+						[&] {
+							// update counters
+							PoolMemoryAllocator::getGlobalFreeListSizes(global_sizes_old);
+							size_t mAllocatedInBlocks =
+									PoolMemoryAllocator::memoryAllocatedInBlocks();
+
+							PoolMemoryAllocator::defragGlobal(); // thread list will not be defrag'ed...
+
+							// so use up those guys first
+							std::vector<size_t> thread_sizes_new;
+							PoolMemoryAllocator::getThreadFreeListSizes(thread_sizes_new);
+							std::vector<OGDFObject<OBJ_SIZE>*> objects2;
+							for (int i = 0; i < thread_sizes_new.at(OBJ_SIZE); ++i) {
+								objects2.push_back(new OGDFObject<OBJ_SIZE>());
+							}
+							PoolMemoryAllocator::getThreadFreeListSizes(thread_sizes_new);
+							AssertThat(thread_sizes_new.at(OBJ_SIZE), Equals(0));
+
+							// now we should be using from the global list
+							for (int i = 0; i < MAX; ++i) {
+								objects.push_back(new OGDFObject<OBJ_SIZE>());
+							}
+							AssertThat(std::is_sorted(objects.begin(), objects.end()), IsTrue());
+
+							std::vector<size_t> global_sizes_new;
+							PoolMemoryAllocator::getGlobalFreeListSizes(global_sizes_new);
+							AssertThat(global_sizes_new.at(OBJ_SIZE) - global_sizes_old.at(OBJ_SIZE),
+									IsGreaterThanOrEqualTo(MAX));
+
+							AssertThat(PoolMemoryAllocator::memoryAllocatedInBlocks(),
+									Equals(mAllocatedInBlocks));
+
+							for (auto object : objects) {
+								delete object;
+							}
+							for (auto object : objects2) {
+								delete object;
+							}
+						},
+#ifdef OGDF_MEMORY_POOL_NTS
+						true
+#else
+						false
+#endif
+				);
+			},
+#ifdef OGDF_MEMORY_MALLOC_TS
+			true
+#else
+			false
+#endif
+	);
 });
