ca80e9f97669fff37663c4b5643f8b22f85440a1
Simon D Fink <finksim@fim.uni-passau.de>
2025-07-22 17:09:30 +0200

    fix FME use of SSE intrinsics

--- include/ogdf/basic/internal/intrinsics.h
+++ include/ogdf/basic/internal/intrinsics.h
@@ -38,4 +38,8 @@
 
 #ifdef OGDF_SSE3_EXTENSIONS
 #	include OGDF_SSE3_EXTENSIONS // IWYU pragma: export
+#else
+// custom flags sometimes set by CI for testing but now surely not available
+#	undef OGDF_FME_KERNEL_USE_SSE
+#	undef OGDF_FME_KERNEL_USE_SSE_DIRECT
 #endif
