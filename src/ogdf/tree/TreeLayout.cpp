907dc8b8f19fd6a03b5060a4d545e8e218cae711
Max Ilsen <milsen@users.noreply.github.com>
2025-07-30 11:08:47 +0200

    Enable TreeLayout on non-arborescence forests

--- src/ogdf/tree/TreeLayout.cpp
+++ src/ogdf/tree/TreeLayout.cpp
@@ -381,7 +381,13 @@ void TreeLayout::call(GraphAttributes& AG) {
 		return;
 	}
 
-	OGDF_ASSERT(isArborescenceForest(tree));
+#ifdef OGDF_DEBUG
+	if (m_selectRoot == RootSelectionType::Source) {
+		OGDF_ASSERT(isArborescenceForest(tree));
+	} else {
+		OGDF_ASSERT(isAcyclicUndirected(tree));
+	}
+#endif
 	OGDF_ASSERT(m_siblingDistance > 0);
 	OGDF_ASSERT(m_subtreeDistance > 0);
 	OGDF_ASSERT(m_levelDistance > 0);
