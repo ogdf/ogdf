038339dfbd07979492cd307802fc3763f31dd943
Simon D Fink <finksim@fim.uni-passau.de>
2026-01-27 15:03:46 +0100

    iwyu

--- src/ogdf/fileformats/GraphMLParser.cpp
+++ src/ogdf/fileformats/GraphMLParser.cpp
@@ -32,7 +32,6 @@
 #include <ogdf/basic/Graph.h>
 #include <ogdf/basic/GraphAttributes.h>
 #include <ogdf/basic/Logger.h>
-#include <ogdf/basic/basic.h>
 #include <ogdf/basic/geometry.h>
 #include <ogdf/basic/graphics.h>
 #include <ogdf/cluster/ClusterGraph.h>
